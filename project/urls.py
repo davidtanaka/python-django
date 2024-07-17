5ebf1182a0aa36a0ec265681b6b68ebdd435a4d3
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-07-17 14:39:16 -0300

    Primeira URL, HttpRequest e HttpResponse

--- project/urls.py
+++ project/urls.py
@@ -16,7 +16,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def myView(request):
+    return HttpResponse('Uma mensagem para alguem especial')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', myView),
 ]
