4ecce7c3b62db025ed88433915d20dcb4e4dcb83
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-07-17 23:57:59 -0300

    Aninhando URLs com path, include e urls.py dos apps do Django

--- project/urls.py
+++ project/urls.py
@@ -15,13 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 from home import views
 from blog import views as views_blog
 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('blog/', views_blog.myView),
-    path('', views.home),
+    path('blog/', include('blog.urls')),
+    path('', include('home.urls')),
 ]
