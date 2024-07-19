9b1c2af70fa6f413676b2bede9ab40570e74e463
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-07-18 23:00:16 -0300

    Configurando templates globais com DIRS + extends para herança de tem…

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,4 @@ from blog import views
 urlpatterns = [
     path('', views.blog),
     path('exemplo/', views.exemplo)
-]
+]   
--- home/views.py
+++ home/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    print('Home')
     return render(
         request,
         'home/index.html'
