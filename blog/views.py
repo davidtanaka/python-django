7591453e5cf442011df31c130b48c564994c9e99
Davi Dias Tanaka <134816317+davidtanaka@users.noreply.github.com>
2024-07-24 23:01:42 -0300

    Configurando um post único no template post.html

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,8 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 from blog.data import posts
+from typing import Any
+
 # Create your views here.
 def blog(request):
     context = {
@@ -14,14 +17,25 @@ def blog(request):
     )
 
 
-def post(request, id):
+def post(request: HttpRequest, post_id: int):
+    found_post: dict[str, Any] | None = None
+
+    for post in posts:
+        if post['id'] == post_id:
+            found_post = post
+            break
+
+    if found_post is None:
+        raise Exception('Post não existe')
+
     context = {
-        'posts': posts
+        'post': found_post,
+        'title': found_post['title'] + ' - '
     }
 
     return render(
         request,
-        'blog/index.html',
+        'blog/post.html',
         context=context
     )
 
